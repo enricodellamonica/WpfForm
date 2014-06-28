4c8260f986ee040286407ec647a12edc284d7c0f
Enrico Della Monica <enricodellamonica@gmail.com>
2014-06-28 11:53:57 +0200

    first

---
+++ WpfForm/Calculator.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfForm {
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        private Double value = 0;
+        private String operation = "";
+        private bool operator_pressed = false;
+        public Calculator() {
+            InitializeComponent();
+            }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+        if(operator_pressed) {
+            Screen.Clear();
+     
+            }
+            var b = (Button)sender;
+            Screen.Text += b.Content; 
+            }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) {
+            var b = (Button)sender;
+            operation = b.Name;
+            value = Double.Parse(Screen.Text);
+            operator_pressed = true;
+            }
+              
+        private void Result_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                switch (operation)
+                {
+                    case "Plus":
+                        Screen.Text = (value + Double.Parse(Screen.Text)).ToString();
+                        break;
+                    case "Minus":
+                        Screen.Text = (value - Double.Parse(Screen.Text)).ToString();
+                        break;
+                    case "Multiply":
+                        Screen.Text = (value * Double.Parse(Screen.Text)).ToString();
+                        break;
+                    case "Divide":
+                        Screen.Text = (value / Double.Parse(Screen.Text)).ToString();
+                        break;
+                    default:
+                        break;
+                }
+                operator_pressed = false;
+
+
+
+            }
+            catch (Exception exc)
+            {
+                Screen.Text = "Error!";
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e) {
+        Screen.Text = "";
+
+
+            }
+
+
+
+        private void Back_Click(object sender, RoutedEventArgs e) {
+            if(Screen.Text != "") {
+                Screen.Text = Screen.Text.Remove(Screen.Text.Length - 1);
+                }
+            }
+
+
+    }
+    }
---
+++ WpfForm/Registration.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfForm {
+    /// <summary>
+    /// Interaction logic for Registration.xaml
+    /// </summary>
+    public partial class Registration : Window {
+        List<string> fields = new List<string>();
+        
+
+        public Registration() {
+            InitializeComponent();
+
+            }
+
+        private void Submit_Click(object sender, RoutedEventArgs e) {
+
+            //if (Directory.Exists(@"C:\Excel"))
+            //{
+            //    StreamWriter writer = new StreamWriter(@"C:\Excel\Temp.txt");
+            //    foreach (var item in fields)
+            //    {
+            //        writer.WriteLine(item);
+            //    }
+
+            //    writer.Close();
+            //}
+            //else
+            //{
+               
+            //   Directory.CreateDirectory(@"C:\Excel");
+            //    StreamWriter writer = new StreamWriter(@"C:\Excel\Temp.txt");
+            //    foreach (var item in fields)
+            //    {
+            //        writer.WriteLine(item);
+            //    }
+               
+            //    writer.Close();
+            //}
+
+
+        
+        File.WriteAllLines(@"C:\Excel\Temp.txt", fields);
+       
+            }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var t = sender as TextBox;
+            fields.Add(t.Text);
+            }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e) {
+            var t = sender as RadioButton;
+            fields.Add(t.Content.ToString());
+
+            }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+            var t = sender as CheckBox;
+            fields.Add(t.Content.ToString());
+
+            }
+
+
+        private void TextBox_LostFocus_1(object sender, RoutedEventArgs e)
+        {
+        var t = sender as TextBox;
+        if(t != null && !t.Text.Contains("@") && !t.Text.Contains(".")) {
+            MessageBox.Show("Please enter a valid email");
+            }
+        }
+
+        private void TextBox_LostFocus_2(object sender, RoutedEventArgs e)
+        {
+            var t = sender as TextBox;
+            if (t != null && t.Text.Length!=10)
+            {
+                MessageBox.Show("Phone number must have 10 digits");
+            }
+            }
+    }
+    }
